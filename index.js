23b885e46a441c059eb91a8e41165557c9fbb422
emimaristan <maristan97@gmail.com>
2023-01-18 15:45:42 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function scrollToAbout() {
+    document.querySelector('#about').scrollIntoView({behavior: 'smooth'});
+}
+function scrollToKnowledges() {
+    document.querySelector('#knowledges').scrollIntoView({behavior: 'smooth'});
+}
+function scrollToProjects() {
+    document.querySelector('#projects').scrollIntoView({behavior: 'smooth'});
+}
+function scrollToContact() {
+    document.querySelector('#contact').scrollIntoView({behavior: 'smooth'});
+}
+function scrollToTop() {
+    document.querySelector('#content').scrollIntoView({behavior: 'smooth'});
+}
