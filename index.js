323d086c7c6672cdce4f8af62506fc64abfb42da
emimaristan <maristan97@gmail.com>
2023-01-23 08:54:57 -0300

    Spinner added

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 function scrollToAbout() {
-    document.querySelector('#about').scrollIntoView({behavior: 'smooth'});
+  document.querySelector("#about").scrollIntoView({ behavior: "smooth" });
 }
 function scrollToKnowledges() {
-    document.querySelector('#knowledges').scrollIntoView({behavior: 'smooth'});
+  document.querySelector("#knowledges").scrollIntoView({ behavior: "smooth" });
 }
 function scrollToProjects() {
-    document.querySelector('#projects').scrollIntoView({behavior: 'smooth'});
+  document.querySelector("#projects").scrollIntoView({ behavior: "smooth" });
 }
 function scrollToContact() {
-    document.querySelector('#contact').scrollIntoView({behavior: 'smooth'});
+  document.querySelector("#contact").scrollIntoView({ behavior: "smooth" });
 }
 function scrollToTop() {
-    document.querySelector('#content').scrollIntoView({behavior: 'smooth'});
+  document.querySelector("#content").scrollIntoView({ behavior: "smooth" });
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+  const loader = document.querySelector(".spinner-download");
+  setTimeout(() => {
+    loader.classList.remove("spinner-download");
+    loader.classList.add("spinner-download-hidden");
+  }, 2000);
+});
